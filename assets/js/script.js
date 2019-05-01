a9d85cdea6269b1b1070f6e0b9b3196eacd6aaed
Felipe <f.perezcisternas@gmail.com>
2019-05-01 18:28:06 -0300

    Se agregan lin del nav y algo de color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 $(function(){
+
+    $("a").click(function(event){
+        if(this.hash !== ""){
+          event.preventDefault();
+    
+          var gato = this.hash;
+    
+          $("html, body").animate({
+            scrollTop: $(gato).offset().top
+          }, 800, function(){
+            window.location.hash = gato;
+          });
+        }
+      });
+
     $(".bujo").click(function(){
         var imagen = $(this).attr("src");
         var Titulo = $(this).attr("alt");
